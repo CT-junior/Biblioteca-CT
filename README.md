5e66abfabb0ea3addaa865ca307172734cf76c61
Jheam Storch Ross <jheam.storch@hotmail.com>
2022-08-31 21:45:25 -0300

    Setup NextAuth, add GoogleProvider

--- README.md
+++ README.md
@@ -2,7 +2,14 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 
 ## Getting Started
 
-First, run the development server:
+1. Create a `.env.local` file at the root of your project and add the client ID and client secret:
+
+```
+GOOGLE_ID=YOUR_CLIENT_ID
+GOOGLE_SECRET=YOUR_CLIENT_SECRET
+```
+
+2. *Next*, run the development server:
 
 ```bash
 npm run dev
---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+  callbacks: {
+    async signIn({ account, profile }) {
+      if (account.provider === "google") {
+        return profile.email_verified && profile.email.endsWith("@ctjunior.com.br")
+      }
+      return true // if there is no specific handling for the provider, sign in is allowed
+    },
+  }
+})
