8c163ac4b525e65496d439f8134643d0ed2449cb
Jheam Storch Ross <jheam.storch@hotmail.com>
2022-08-31 22:20:03 -0300

    Setup Google signIn through Button in page

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,10 +1,11 @@
 import { Box, Button, Center, Divider, Flex, Stack } from "@chakra-ui/react";
 import type { NextPage } from "next";
+import { getProviders, signIn } from "next-auth/react"
 
 import Image from "next/image";
 import { FcGoogle } from "react-icons/fc";
 
-const SignIn: NextPage = () => {
+const SignIn: NextPage = ({ providers }) => {
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Box bgColor="#F6F6F6" w="1120px" h="300" borderRadius={40} shadow="lg">
@@ -28,6 +29,7 @@ const SignIn: NextPage = () => {
             pr="120px"
           >
             <Button
+              onClick={() => signIn(providers['google'].id)}
               size="lg"
               leftIcon={<FcGoogle size={24} />}
               bg="white"
@@ -49,3 +51,10 @@ const SignIn: NextPage = () => {
 };
 
 export default SignIn;
+
+export async function getServerSideProps() {
+  const providers = await getProviders()
+  return {
+    props: { providers },
+  }
+}
