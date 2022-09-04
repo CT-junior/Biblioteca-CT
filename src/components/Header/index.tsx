27cd0b6395733d5e45b02f37cd31298accfce8b8
Héber Lima Silva <heberlimasilva@gmail.com>
2022-09-04 11:37:06 -0300

    Modal de confirmação de signOut

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,54 +1,71 @@
-import { Flex, HStack, Icon, IconButton} from "@chakra-ui/react";
+import { Flex, HStack, Icon, IconButton, useDisclosure} from "@chakra-ui/react";
 import Image from "next/image";
+import { useRef } from "react";
+
+
+import { useSidebar } from "../../hooks/sidebar";
+import { toggleFixedSidebar } from "../../store/sidebar/actions";
+
+import { Profile } from "./Profile";
+import { SearchBox } from "./SearchBox";
 
 import { HiOutlineMenu } from "react-icons/hi";
 import { FiLogOut } from "react-icons/fi";
 
 import biblioctecaLogo from "../../assets/images/bibliocteca-horizontal-logo.svg"
+import { SignOutModal } from "../SignOutModal";
 
-import { useSidebar } from "../../hooks/sidebar";
-import { toggleFixedSidebar } from "../../store/sidebar/actions";
-import { Profile } from "./Profile";
-import { SearchBox } from "./SearchBox";
 
 export function Header() {
   const { isFixed } = useSidebar();
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const cancelRef: any = useRef();
 
   return (
-    <Flex
-      as="header"
-      w="100%"
-      h="16"
-      mx="auto"
-      px="5"
-      align="center"
-      justify="space-between"
-      borderBottomColor="blackAlpha.200"
-      borderBottomWidth="1px"
-      borderBottomStyle="solid"
+    <>
+      <Flex
+        as="header"
+        w="100%"
+        h="16"
+        mx="auto"
+        px="5"
+        align="center"
+        justify="space-between"
+        borderBottomColor="blackAlpha.200"
+        borderBottomWidth="1px"
+        borderBottomStyle="solid"
 
-    >
-      <HStack gap="8" >
-        <IconButton 
-          aria-label="Open navigation"
-          icon={<Icon as={HiOutlineMenu} color="gray.700"/>}
-          fontSize="24px"
-          borderRadius="50%"
-          onClick={toggleFixedSidebar}
-          bg={isFixed ? "blackAlpha.200" : ""}
-        ></IconButton>
-        
-        <Image src={biblioctecaLogo} alt="Logo da Bibliocteca" />
-      </HStack>
+      >
+        <HStack gap="8" >
+          <IconButton 
+            aria-label="Open navigation"
+            icon={<Icon as={HiOutlineMenu} color="gray.700"/>}
+            fontSize="24px"
+            borderRadius="full"
+            onClick={toggleFixedSidebar}
+            bg={isFixed ? "blackAlpha.200" : ""}
+          />
+          
+          <Image src={biblioctecaLogo} alt="Logo da Bibliocteca" width="198.4px" height="28.8"/>
+        </HStack>
 
-      <SearchBox />
+        <SearchBox />
 
-      <HStack spacing="5">
-        <Profile />
-        <Icon as={FiLogOut} fontSize="2xl" color="gray.700"/>
-      </HStack>
-     
+        <HStack spacing="5">
+          <Profile />
+          <IconButton 
+            aria-label="Open signOut"
+            icon={<Icon as={FiLogOut} color="gray.700"/>}
+            fontSize="24px"
+            borderRadius="full"
+            bg="unset"
+            onClick={onOpen}
+          />
+        </HStack>
+      
 
-    </Flex>
+      </Flex>
+      <SignOutModal isOpen={isOpen} onClose={onClose} leastDestructiveRef={cancelRef} children/>
+    </>
   );
 }
