4a0059ced972f2089f9cb1461b1f6f9b26ef503a
Héber Lima Silva <heberlimasilva@gmail.com>
2022-09-07 08:17:57 -0300

    Ajustando responsividade

--- src/pages/library-manager.tsx
+++ src/pages/library-manager.tsx
@@ -51,60 +51,57 @@ const LibraryManager: NextPage = () => {
         <Sidebar />
         <Box w="100%" px="8" py="8">
           <Flex
-            align={["flex-start", "flex-start", "center"]}
-            justify="space-between"
+            align={["flex-start","center"]}
             aria-label="second-header"
             pb="6"
             borderBottom="1px"
             borderColor="gray.200"
-            alignItems={["flex-start","flex-end"]}
             gap="2"
-            direction={["column", "row"]}
+            direction={["column", "column","row"]}
           >
-            
-
-              <Flex direction={["column", "column", "row"]} gap="2">
-                <Heading as="h1" fontSize="xl">
-                  Gerenciador de biblioteca
-                </Heading>
-                <HStack>
-                  <Button
-                    onClick={onOpen}
-                    size="sm"
-                    leftIcon={<Icon as={HiPlus} color="white" />}
-                    borderRadius="full"
-                    {...colorSchemeOrangeCt}
-                  >
-                    Adicionar Livro
-                  </Button>
-                  {isWideVersion && (
-                    <InputGroup w="32" size="sm">
-                      <InputLeftElement
-                        children={<Icon as={HiSearch} color="orange.ct" />}
-                      />
-                      <Input
-                        placeholder="Pequisar"
-                        variant="outline"
-                        borderRadius="full"
-                        {...colorSchemeOrangeCtOutline}
-                      />
-                    </InputGroup>
-                  )}
-                </HStack>
-              </Flex>
-
+            <Heading as="h1" fontSize="xl" >
+              Gerenciador de biblioteca
+            </Heading>
+            <HStack>
               <Button
-                variant="outline"
+                onClick={onOpen}
                 size="sm"
-                rightIcon={<Icon as={HiCloudDownload} color="orange.ct" />}
-                {...colorSchemeOrangeCtOutline}
+                width="100%"
+                maxW="40"
+                leftIcon={<Icon as={HiPlus} color="white" />}
                 borderRadius="full"
-                fontWeight="sm"
-                px="8"
+                {...colorSchemeOrangeCt}
               >
-                Exportar
+                Adicionar Livro
               </Button>
-           
+              {isWideVersion && (
+                <InputGroup w="32" size="sm">
+                  <InputLeftElement
+                    children={<Icon as={HiSearch} color="orange.ct" />}
+                  />
+                  <Input
+                    placeholder="Pequisar"
+                    variant="outline"
+                    borderRadius="full"
+                    {...colorSchemeOrangeCtOutline}
+                  />
+                </InputGroup>
+              )}
+            </HStack>
+            <Button
+              variant="outline"
+              size="sm"
+              rightIcon={<Icon as={HiCloudDownload} color="orange.ct" />}
+              {...colorSchemeOrangeCtOutline}
+              borderRadius="full"
+              width="100%"
+              maxW="28"
+              fontWeight="sm"
+              px="8"
+              ml={["unset","unset","auto"]}
+            >
+              Exportar
+            </Button>
           </Flex>
           <TableContainer fontSize="sm">
             <Table variant="simple">
