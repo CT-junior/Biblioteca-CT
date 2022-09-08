0b496c9ad06ffa0da0602057837c28f900adc973
Héber Lima Silva <heberlimasilva@gmail.com>
2022-09-08 14:11:50 -0300

    Apagando código comentado

--- src/components/ActiveLink.tsx
+++ src/components/ActiveLink.tsx
@@ -20,12 +20,6 @@ export function ActiveLink({
     isActive = true;
   }
 
-  // if(!shouldMatchExactHref && 
-  //   (asPath.startsWith(String(rest.href)) || 
-  //   asPath.startsWith(String(rest.as)))){
-  //       isActive = true
-  //   }
-
   return (
     <Link {...rest} >
       {cloneElement(children, {
--- src/components/TableLibraryManager/index.tsx
+++ src/components/TableLibraryManager/index.tsx
@@ -6,30 +6,18 @@ import {
 
 import { ReactNode } from "react";
 
-import { MoreSettingsPopover } from "../MoreSettingsPopover";
 import { Thead } from "./Thead";
 
 interface TableProps extends TableChakraProps {
   children?: ReactNode;
-
 }
 
-export function TableLibraryManager({ children,...rest }: TableProps) {
+export function TableLibraryManager({ children, ...rest }: TableProps) {
   return (
     <TableContainer fontSize="sm" {...rest}>
       <TableChakra variant="simple">
-          <Thead />
-            {children}
-
-            {/* <Td display="revert">Sussuros na floresta</Td>
-            <Td display={["none", "none", "none", "revert"]}>Volume 1</Td>
-            <Td display={["none", "none", "revert"]}>Maria Silveira</Td>
-            <Td display={["none", "revert"]}>Suspense</Td>
-            <Td display={["none", "none", "revert"]}>04 de setembro, 2022</Td>
-            <Td textAlign={["end", "end", "center"]}>
-              <MoreSettingsPopover />
-            </Td> */}
-         
+        <Thead />
+        {children}
       </TableChakra>
     </TableContainer>
   );
