14d127a8299fbefce071adabd78bd3c0cc4feac5
Héber Lima Silva <heberlimasilva@gmail.com>
2022-09-09 16:21:30 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,13 +3,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-
-}
-
-module.exports = {
-  nextConfig,
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
 
 }
+
+module.exports =  nextConfig;
