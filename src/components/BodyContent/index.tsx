2d3f26139b4ef266bbce981b7244d39fb3a3bd39
Héber Lima Silva <heberlimasilva@gmail.com>
2022-10-07 18:11:20 -0300

    Update bodycontent formatação

--- src/components/BodyContent/index.tsx
+++ src/components/BodyContent/index.tsx
@@ -10,40 +10,40 @@ import { Header } from "../Header";
 import { Sidebar } from "../Sidebar";
 
 interface Props {
-	children?: ReactNode;
+  children?: ReactNode;
 }
 
 export const BodyContent = ({ children }: Props) => {
-	const router = useRouter();
+  const router = useRouter();
 
-	const isOpenSidebar = useSidebar().isOpen;
+  const isOpenSidebar = useSidebar().isOpen;
 
-	const isSideBarDrawer = useBreakpointValue({
-		base: true,
-		md: false,
-	});
+  const isSideBarDrawer = useBreakpointValue({
+    base: true,
+    md: false,
+  });
 
-	if (router.pathname === "/auth/signin") {
-		return <Box>{children}</Box>;
-	}
+  if (router.pathname === "/auth/signin") {
+    return <Box>{children}</Box>;
+  }
 
-	return (
-		<Flex flexDirection="column">
-			<Box h="16">
-				<Header />
-			</Box>
-			<Flex>
-				<Sidebar />
-				<Box
-					w="100%"
-					px="8"
-					py="8"
-					ml={isSideBarDrawer ? "0" : isOpenSidebar ? "64" : "20"}
-					transition="0.2s"
-				>
-					{children}
-				</Box>
-			</Flex>
-		</Flex>
-	);
+  return (
+    <Flex flexDirection="column">
+      <Box h="16">
+        <Header />
+      </Box>
+      <Flex>
+        <Sidebar />
+        <Box
+          w="100%"
+          px="8"
+          py="8"
+          ml={isSideBarDrawer ? "0" : isOpenSidebar ? "64" : "20"}
+          transition="0.2s"
+        >
+          {children}
+        </Box>
+      </Flex>
+    </Flex>
+  );
 };
